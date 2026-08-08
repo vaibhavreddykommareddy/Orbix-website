4769904d8380706ad34dc3ee5fa4bdd8e639578e
vaibhavreddykommareddy <vaibhavreddykommareddy@gmail.com>
2026-08-08 18:20:28 +0530

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
